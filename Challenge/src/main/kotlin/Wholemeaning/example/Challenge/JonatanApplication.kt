1b4f1c9c545c4f5ec25b006153d2a6aac029bdbe
jonatan_montenegro <mrmontenegro@gmail.com>
2022-08-11 22:25:50 -0300

    first commit

---
+++ Challenge/src/main/kotlin/Wholemeaning/example/Challenge/JonatanApplication.kt
@@ -0,0 +1,11 @@
+package Wholemeaning.example.Challenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JonatanApplication
+
+fun main(args: Array<String>) {
+	runApplication<JonatanApplication>(*args)
+}
---
+++ Challenge/src/test/kotlin/Wholemeaning/example/Challenge/JonatanApplicationTests.kt
@@ -0,0 +1,13 @@
+package Wholemeaning.example.Challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JonatanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
